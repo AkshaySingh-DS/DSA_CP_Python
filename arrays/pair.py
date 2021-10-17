cd911d81192e237e9bd4fb49ce7a4249f1b30f31
Akshay_Singh <singhakshay.etw69@gmail.com>
2021-10-17 20:10:19 +0530

    arrays added

---
+++ arrays/pair.py
@@ -0,0 +1,35 @@
+def pair(arr, s):
+    hashMap = {i: True for i in arr}
+    # this might not work for some cases like if we have duplicate element (2,2) or just
+    # one 2, and sum = 4, then it'll return [2, 2] which is incorrect
+
+    # logic
+    for i in range(len(arr)):
+        if hashMap.get(s - arr[i]):
+            return [arr[i], s-arr[i]]
+
+    else:
+        return [-1, -1]
+
+def pair2(arr , sum): # Time O(N) and space O(N)
+    hashMap = {}
+
+    # logic
+    for i in range(len(arr)):
+        if hashMap.get(sum-arr[i]):
+
+            return [arr[i],sum-arr[i]]
+        else:
+
+            hashMap[arr[i]] = True
+    else:
+
+        return [-1, -1]
+
+
+# NOTE - Other solution brute force search O(N^2) and one optimization after sorting with binary search will takes O(N log N)
+if __name__ =="__main__":
+    #data = [10, 5, 2, 3, 6, 9, 11]
+    data = [10, 5, 2, 3, -6, 9, 11]
+    sum = 4
+    print(pair2(data, sum))
