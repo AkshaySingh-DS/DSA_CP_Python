9cb159b76a9cf4625ef900b534cf47ff418557e7
Akshay_Singh <singhakshay.etw69@gmail.com>
2021-12-04 22:31:12 +0530

    array intersection

---
+++ arrays/array_intersection.py
@@ -0,0 +1,45 @@
+
+"""
+Problem Statement : Codestdio
+https://www.codingninjas.com/codestudio/problems/intersection-of-2-arrays_1082149?leftPanelTab=1?source=youtube&campaign=LoveBabbar_Codestudiovideo1&utm_source=youtube&utm_medium=affiliate&utm_campaign=LoveBabbar_Codestudiovideo1
+TIME COMPLEXITY : O(max(M, N))
+"""
+import  sys
+def findArrayIntersection(arr: list, n: int, brr: list, m: int):
+    # Write your code here
+    # Return a list containing all the common elements in arr and brr.
+    intersec = []
+    
+    p = 0 ; q = 0
+    flag = False
+    
+    while p < n and q < m:
+        if arr[p]  == brr[q]:
+            intersec.append(arr[p])
+            p += 1 ; q += 1
+            flag = True
+        elif arr[p] < brr[q]:
+            p += 1
+        elif arr[p] > brr[q]:
+            q += 1
+            
+    #if there at least one element is common 
+    if flag:
+        return intersec
+   	
+    return -1
+    
+
+def main():
+
+    #will take fast input in console/terminal than input()
+    t = int(sys.stdin.readline().rstrip())
+    while t > 0:
+        n, m = map(int, sys.stdin.readline().rstrip().split())
+        arr = list(map(int, sys.stdin.readline().rstrip().split()))
+        brr = list(map(int, sys.stdin.readline().rstrip().split()))
+        print(findArrayIntersection(arr, n, brr, m))
+        t -= 1
+
+if __name__ == "__main__":
+    main()
