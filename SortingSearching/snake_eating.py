dab74f29996d88b46a4e026535b2fc465fa0f265
Akshay_Singh <singhakshay.etw69@gmail.com>
2021-12-12 19:32:36 +0530

    snake eating

--- SortingSearching/snake_eating.py
+++ SortingSearching/snake_eating.py
@@ -5,7 +5,8 @@ https://www.codechef.com/problems/SNAKEEAT
 TIME COMPLEXITY: O(N log N) + O(N)
 
 """
-def maxSnakes(snakeslength, n, q):
+import sys
+""" def maxSnakes(snakeslength, n, q):
     left = 0 ; right = n - 1
     ub = -1
     #logic
@@ -38,20 +39,84 @@ def maxSnakes(snakeslength, n, q):
             ub = j
             j = ub -1
     
-    return count 
+    return count  """
+
+
+
+def getPrefixSum(prefixSum, mid, M):
+
+    #boundry case
+    if mid == 0:
+        return prefixSum[M]
     
+    return prefixSum[M] - prefixSum[mid-1]
 
-def main():
-    t = int(input())
+def maxSnakesSurvived(snakesLen, nsnakes, reqLen, prefixSum):
+    #get all the snakes first that can be survived whose length >= reqLen
 
-    while t > 0:
+    left, right = 0, nsnakes - 1
+    upperB = -1
 
-        n, q = map(int, input().split())
-        snakeslength = sorted(list(map(int, input().split())))
-        for _ in range(q):
-            k = int(input())
-            print(maxSnakes(snakeslength, n, k))
-        t -= 1
+    #below code can be solved used bisect module 
+    while left <= right:
 
-if __name__ == "__main__":
-    main()
+        mid = (left + right)//2
+
+        if snakesLen[mid] < reqLen:
+            upperB= mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    #Boundary case all snakes len >= reqLen
+    if upperB == -1:
+        return n
+    
+    #else
+    ans = n - (upperB + 1)
+
+    #try to get all the left snakes from 0 to upperB that can make length reqLen by eating up each other
+
+    """
+    first we'll try, if all the snakes mid to upperBound(M) i.e. suffix that can be possible to eatup all left out snakes(0 to mid-1)
+    if possible then we'll try to expand the suffix size i.e. will go the left else right i.e. reduce suffix size
+    """ 
+
+    left = 0; right = upperB
+
+    while left <= right:
+
+
+        mid = (left + right)//2
+
+        #if this mid is good i.e. try to eatupu all mid left snakes from the mid+1 to right snakes (#snakes >= sigma(reqLen - snakesLen) from i = mid to)
+        totalSnakes = reqLen * (upperB - mid + 1) - getPrefixSum(prefixSum, mid, upperB)
+
+        if mid >= totalSnakes:
+            #maximize suffix size i.e go to left
+            ans += totalSnakes
+            right = mid - 1
+        else:
+            #diminish the suffiz size
+            left = mid + 1
+        
+    return ans
+
+
+t = int(sys.stdin.readline().strip())
+
+while t > 0:
+
+    n, q = map(int,  sys.stdin.readline().strip().split())
+    snakeslength = sorted(list(map(int, sys.stdin.readline().strip().split())))
+
+    prefixSum = [0] * n
+    for i in range(n):
+        prefixSum[i] = prefixSum[i - 1] + snakeslength[i]
+
+
+    for _ in range(q):
+        k = int(sys.stdin.readline().strip())
+        print(maxSnakesSurvived(snakeslength, n, k, prefixSum))
+
+    t -= 1
