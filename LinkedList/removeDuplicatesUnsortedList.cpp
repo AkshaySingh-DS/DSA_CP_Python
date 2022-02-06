d77198814ce7a5d033e3a6924ee4f8bc6df42f56
Akshay_Singh <singhakshay.etw69@gmail.com>
2022-02-07 00:56:33 +0530

    remove duplicates from unsorted list

---
+++ LinkedList/removeDuplicatesUnsortedList.cpp
@@ -0,0 +1,79 @@
+/*Problem Statement :https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1
+or, https://leetcode.com/problems/remove-duplicates-from-an-unsorted-linked-list/
+Time Complexity : 
+Space Comlpexity :    
+*/ 
+
+//Method-1 Brute force O(n^2) time, O(1) space
+//Method-2 Sort the list, then remove duplicates in sorted list O(NlogN) time, O(1) space
+//Method-3 Use the map method O(N) time, O(N) space
+
+//method-3
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) {
+     // your code goes here
+        Node *prev = NULL, *curr = head;
+        
+        //this map is going to be our visited list of node->data
+        unordered_map <int, bool> map;
+        
+        while(curr != NULL){
+            
+            //duplicate found -> remove the duplicate 
+            if(map.find(curr -> data) != map.end()){
+                
+                prev -> next = curr -> next;
+                curr -> next = NULL;
+                delete curr;
+                curr = prev -> next;
+                
+            }
+            //unseen data value then keep this unseen in map for future to find the duplicates
+            else{
+                map[curr -> data] = true;
+                prev = curr;
+                curr = curr -> next;
+            }
+        }
+        
+        return head;
+    }
+};
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) {
+     // your code goes here
+        Node *prev = NULL, *curr = head;
+        
+        //this map is going to be our visited list of node->data
+        unordered_map <int, bool> map;
+        
+        while(curr != NULL){
+            
+            //duplicate found -> remove the duplicate 
+            if(map.find(curr -> data) != map.end()){
+                
+                prev -> next = curr -> next;
+                curr -> next = NULL;
+                delete curr;
+                curr = prev -> next;
+                
+            }
+            //unseen data value then keep this unseen in map for future to find the duplicates
+            else{
+                map[curr -> data] = true;
+                prev = curr;
+                curr = curr -> next;
+            }
+        }
+        
+        return head;
+    }
+};
+
+
