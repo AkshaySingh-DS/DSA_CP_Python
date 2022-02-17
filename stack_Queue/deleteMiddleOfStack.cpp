b874af2d2e1cb0f0e3445261a523ce3ec62ce05c
Akshay_Singh <singhakshay.etw69@gmail.com>
2022-02-18 00:11:17 +0530

    delete middle of stack

---
+++ stack_Queue/deleteMiddleOfStack.cpp
@@ -0,0 +1,32 @@
+/* 
+Problem Statement : https://www.codingninjas.com/codestudio/problems/delete-middle-element-from-stack_985246
+Time Complexity: O(N)
+Space Complexity: O(N)
+
+Better approach using two pointer approaching using swapping only in O(N/2) single pass inistead two passes.
+*/
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void solve(stack<int>&inputStack, int cnt, int size){
+    //base case
+    if (cnt == size/2){
+        inputStack.pop();
+    	return; 
+    }
+    
+    //recursive case
+    int k = inputStack.top();
+    inputStack.pop();
+    
+    solve(inputStack, cnt + 1, size);
+    
+    //push the above element
+    inputStack.push(k);
+}
+void deleteMiddle(stack<int>&inputStack, int N){
+	  
+    
+    solve(inputStack, 0, N);   
+}
