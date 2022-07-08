12d4657b0bb5bde34be88cb729606b05f80efbbf
Akshay_Singh <singhakshay.etw69@gmail.com>
2022-07-08 09:16:39 +0530

    count all paths  Rat in a Maze

--- RecursionBacktracking/CountPathsRateInMaze.cpp
+++ RecursionBacktracking/CountPathsRateInMaze.cpp
@@ -5,16 +5,19 @@ Value 0 at a cell in the matrix represents that it is blocked and rat cannot mov
 a cell in the matrix represents that rat can be travel through it.
 
 NOTE: Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat 
-cannot move to any other cell. */
+cannot move to any other cell. 
+
+GFG LINK: https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1
+
+*/
 
 #include <iostream>
 using namespace std;
 
-
-int possiblePathsToReach(int i, int j, int arr[][1000], int n, int vis[][1000]){
+int possiblePathsToReach(int i, int j, int arr[][1000], int n, int m, int vis[][1000]){
 
     //base case
-    if (i == n-1 and j == n-1)
+    if (i == n-1 and j == m-1)
         return 1;
     
     //rec case
@@ -24,7 +27,7 @@ int possiblePathsToReach(int i, int j, int arr[][1000], int n, int vis[][1000]){
     if(i+1 < n and arr[i+1][j] == 1 and vis[i+1][j] == 0){
         //visit this index
         vis[i][j] = 1; //don't visit this cell again in further calls not untill and unless this path is included   
-        ways += possiblePathsToReach(i+1, j, arr, n, vis);
+        ways += possiblePathsToReach(i+1, j, arr, n, m, vis);
         vis[i][j] = 0;////bactrack step
 
     }
@@ -33,15 +36,15 @@ int possiblePathsToReach(int i, int j, int arr[][1000], int n, int vis[][1000]){
     if(j-1 >= 0 and arr[i][j-1] == 1 and vis[i][j-1] == 0){
         //visit this index
         vis[i][j] = 1;
-        ways += possiblePathsToReach(i, j-1, arr, n, vis);
+        ways += possiblePathsToReach(i, j-1, arr, n, m, vis);
         vis[i][j] = 0;////bactrack step
     }
 
     //right move
-    if(j+1 < n and arr[i][j+1] == 1 and vis[i][j+1] == 0){
+    if(j+1 < m and arr[i][j+1] == 1 and vis[i][j+1] == 0){
         //visit this index
         vis[i][j] = 1; 
-        ways += possiblePathsToReach(i, j+1, arr, n, vis);
+        ways += possiblePathsToReach(i, j+1, arr, n, m, vis);
         vis[i][j] = 0; ////bactrack step
     }
 
@@ -49,7 +52,7 @@ int possiblePathsToReach(int i, int j, int arr[][1000], int n, int vis[][1000]){
     if(i-1 >= 0 and arr[i-1][j] == 1 and vis[i-1][j] == 0){
         //visit this index
         vis[i][j] = 1;
-        ways += possiblePathsToReach(i-1, j, arr, n, vis);
+        ways += possiblePathsToReach(i-1, j, arr, n, m, vis);
         vis[i][j] = 0; //bactrack step
     }
     
@@ -62,16 +65,14 @@ int main(){
     
     int maze[1000][1000];
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < m; j++)
             cin >> maze[i][j];
     }
-
     int visited [1000][1000]= {0};
-    
     int total_ways;
     
     if (maze[0][0] != 0)
-        total_ways = possiblePathsToReach(0, 0, maze, n, visited);
+        total_ways = possiblePathsToReach(0, 0, maze, n, m, visited);
     
     else cout << "Not possible since the entry is blocked" << endl;
     
